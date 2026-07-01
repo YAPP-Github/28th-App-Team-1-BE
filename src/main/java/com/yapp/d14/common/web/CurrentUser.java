1f9c98b78b29414bbad242e91aafd695a6c29ee1
Jina Ham <hamjina61@gmail.com>
2026-07-01 16:07:21 +0900

    feat: @CurrentUser 커스텀 어노테이션 및 ArgumentResolver 추가

---
+++ src/main/java/com/yapp/d14/common/web/CurrentUser.java
@@ -0,0 +1,11 @@
+package com.yapp.d14.common.web;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentUser {
+}
