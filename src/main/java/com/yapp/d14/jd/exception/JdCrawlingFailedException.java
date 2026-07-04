596f9d247c96342a1c61bb0c9a71d2793ae47293
Jina Ham <hamjina61@gmail.com>
2026-07-04 15:33:38 +0900

    docs: JdCrawlingFailedException 의도 설명 주석 추가
    - BusinessException 미상속, Service 내부에서만 처리돼야 함을 명시

--- src/main/java/com/yapp/d14/jd/exception/JdCrawlingFailedException.java
+++ src/main/java/com/yapp/d14/jd/exception/JdCrawlingFailedException.java
@@ -1,5 +1,8 @@
 package com.yapp.d14.jd.exception;
 
+// BusinessException을 상속하지 않는 내부 신호용 예외. JdContentFetcher 구현체가 던지면
+// JdValidateService가 잡아 JdCrawlResult.failure(CRAWLING_FAILED)로 변환해야 하며,
+// GlobalExceptionHandler까지 전파되어서는 안 된다.
 public class JdCrawlingFailedException extends RuntimeException {
 
     public JdCrawlingFailedException(String message) {
